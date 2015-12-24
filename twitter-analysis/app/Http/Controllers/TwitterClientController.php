15912ee76b0d19927f60f15322a5eb1f32a00d0b
nadlerjessie <nadler.jessie@gmail.com>
2015-12-24 11:27:21 -0500

    sets up structure for mongo collection

--- twitter-analysis/app/Http/Controllers/TwitterClientController.php
+++ twitter-analysis/app/Http/Controllers/TwitterClientController.php
@@ -1,23 +1,34 @@
 <?php
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class TwitterClientController extends Controller
 {
   public function getTweets() {
-    $client = new Client(['base_uri' => 'https://api.twitter.com/', 'defaults' => ['auth' => 'oauth']]);
-    // .1/statuses/user_timeline.json/
-    $oauth = [
-      'consumer_key' => env('TWITTER_CONSUMER_KEY'),
-      'consumer_secret' => env('TWITTER_CONSUMER_SECRET'),
-      'token' => env('TWITTER_ACCESS_TOKEN'),
-      'token_secret' => env('TWITTER_ACCESS_TOKEN_SECRET')
-      ];
 
-    $res = $client->get('1.1/statuses/user_timeline.json?screen_name=FlatironSchool&count=2')->getBody();
-    // screen_name=FlatironSchool&count=2
-    print_r($res->json());
+    $tweets = \Twitter::getUserTimeline(['screen_name' => 'FlatironSchool','count' => 50, 
+      'format' => 'json']);
+    $tweet_array = json_decode($tweets);
+    $link = \Twitter::linkTweet($tweet_array[0]);
+    print_r('');
+    print_r('');
+    print_r($tweet_array[1]);
+
+    // $m = new MongoClient();
+    // $collection = $m->selectCollection('tweets');
+    // db.tweets.insert() {
+    //   'text' : $tweet->text,
+    //   'link' : // custom function,
+    //   'retweet_count' : $tweet->retweet_count,
+    //   'time' : $tweet->created_at,
+    //   'favorite_count' : $tweet->favorite_count,
+    //   'hashtag_count' : count($tweet_array[1]->entities->hashtags),
+    //   'retweet' : // custom function 
+    // }
   }
     
 }
