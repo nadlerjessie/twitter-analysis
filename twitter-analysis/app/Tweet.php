c46c2fec6e14031c25c25a212773227e22077863
nadlerjessie <nadler.jessie@gmail.com>
2015-12-25 17:18:33 -0500

    adds attribute reader methods and updates tweet index template

--- twitter-analysis/app/Tweet.php
+++ twitter-analysis/app/Tweet.php
@@ -8,11 +8,32 @@ class Tweet extends Eloquent
 {
     protected $connection = 'mongodb';
 
-    public $text;
-    public $link;
-    public $retweet_count;
-    public $time;
-    public $favorite_count;
-    public $hashtag_count;
-    public $retweet;
+    public function text(){
+        return $this->getAttributes()['text'];
+    }
+
+    public function link(){
+        return $this->getAttributes()['link'];
+    }
+
+    public function retweet_count(){
+        return $this->getAttributes()['retweet_count'];
+    }
+
+    public function time(){
+        return $this->getAttributes()['time'];
+    }
+
+    public function favorite_count(){
+        return $this->getAttributes()['favorite_count'];
+    }
+
+    public function hashtag_count(){
+        return $this->getAttributes()['hashtag_count'];
+    }
+
+    public function retweet(){
+        return $this->getAttributes()['retweet'];
+    }
+
 }
--- twitter-analysis/resources/views/tweets/index.blade.php
+++ twitter-analysis/resources/views/tweets/index.blade.php
@@ -5,7 +5,7 @@
   <?php $i = 1 ?>
   @foreach($tweets as $key => $tweet)
     <?php if( $tweet->getAttribute('text') ) {?>
-      <p><h4>{{$i}}: {{ $tweet->getAttribute('text') }} -- {{ $tweet->getAttribute('favorite_count') }} Favorites -- {{ $tweet->getAttribute('retweet_count') }} RTs</h4></p>
+      <p><h4>{{$i}}: {{ $tweet->text()}} -- {{ $tweet->favorite_count() }} Favorites -- {{ $tweet->retweet_count() }} RTs</h4></p>
     <?php 
       $i += 1;
       } 
